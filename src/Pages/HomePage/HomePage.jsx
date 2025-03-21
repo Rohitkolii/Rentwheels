cad7e58f6f5bb87a8e9f0ec131f353fe43ff01e2
Roohit koli <rohitkolisd@gmail.com>
2025-03-21 17:57:58 +0530

    final commit 2

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -28,44 +28,6 @@ const HomePage = () => {
     <>
         <Header />
         <TechStack />
-        <section className={Styles.detailscon}>
-          <div className={Styles.detailsconinner}>
-            <div className={Styles.row1}>
-              <div>
-                <h1>Explore Our Fleet <br /> Find the Perfect Ride for You!</h1>
-              </div>
-              <div>
-                <p className='sub'>At RentWheels, we make car rentals seamless, affordable, and hassle-free. Whether you need a car for a weekend getaway, a business trip, or everyday commuting, we’ve got the perfect ride for you. With a wide range of vehicles, flexible rental plans, and easy booking, your journey starts here. Rent. Ride. Repeat!</p>
-              </div>
-            </div>
-
-            <div className={Styles.row2}>
-
-              <div className={Styles.row2_first}>
-                <img src="images/car1.png" alt="" />
-              </div>
-
-              <div className={Styles.row2inner}>
-                <div className={Styles.row2inner_1}>
-                  <img src="images/bg3.jpg" alt="" />
-                </div>
-
-                <div className={Styles.row2inner_2}>
-                  <div>
-                    <img src="images/singlecar.jpg" alt="" />
-                  </div>
-                  <div>
-                    <img src="images/bg2.jpg" alt="" />
-                  </div>
-                  <div>
-                    <img src="images/bg1.jpg" alt="" />
-                  </div>
-                    </div>
-                  </div>
-              </div>
-          </div>
-        </section>
-
         <section className={Styles.latestCon}>
           <div className={Styles.latestConInn}>
           <h1>Latest Added</h1>
@@ -107,7 +69,43 @@ const HomePage = () => {
             </div>
           </div>
         </section>
-        
+        <section className={Styles.detailscon}>
+          <div className={Styles.detailsconinner}>
+            <div className={Styles.row1}>
+              <div>
+                <h1>Explore Our Fleet <br /> Find the Perfect Ride for You!</h1>
+              </div>
+              <div>
+                <p className='sub'>At RentWheels, we make car rentals seamless, affordable, and hassle-free. Whether you need a car for a weekend getaway, a business trip, or everyday commuting, we’ve got the perfect ride for you. With a wide range of vehicles, flexible rental plans, and easy booking, your journey starts here. Rent. Ride. Repeat!</p>
+              </div>
+            </div>
+
+            <div className={Styles.row2}>
+
+              <div className={Styles.row2_first}>
+                <img src="images/car1.png" alt="" />
+              </div>
+
+              <div className={Styles.row2inner}>
+                <div className={Styles.row2inner_1}>
+                  <img src="images/bg3.jpg" alt="" />
+                </div>
+
+                <div className={Styles.row2inner_2}>
+                  <div>
+                    <img src="images/singlecar.jpg" alt="" />
+                  </div>
+                  <div>
+                    <img src="images/bg2.jpg" alt="" />
+                  </div>
+                  <div>
+                    <img src="images/bg1.jpg" alt="" />
+                  </div>
+                    </div>
+                  </div>
+              </div>
+          </div>
+        </section>
         <EngageModule />
         <Footer />
     </>
